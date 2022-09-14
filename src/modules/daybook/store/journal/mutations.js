1a7ab60abc64d87e4e3c1fc91c62dc911717112e
Miguel Covarrubias <ing.miguelcovarrubias@gmail.com>
2022-09-14 00:26:23 -0500

    First Commit

---
+++ src/modules/daybook/store/journal/mutations.js
@@ -0,0 +1,16 @@
+// export const myMutation = ( state ) => {
+
+// }
+
+//Agregar las entradas que vienen de la base de datos por medio de las acciones
+export const setEntries = ( state ) => {
+
+}
+
+export const addEntry = ( state ) => {
+
+}
+
+export const updateEntry = ( state ) => {
+
+}
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(), //121345642132145
+            date: new Date().toDateString(), //Sat 2, Mayo
+            text: 'Id qui amet magna fugiat minim veniam est sunt nostrud.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 10000, //121345642132145
+            date: new Date().toDateString(), //Sat 2, Mayo
+            text: 'Id qui amet magna fugiat minim veniam est sunt nostrud. Fugiat nulla velit amet magna eu occaecat voluptate eu anim do eu est esse do.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 2000, //121345642132145
+            date: new Date().toDateString(), //Sat 2, Mayo
+            text: 'Id qui amet magna fugiat minim veniam est sunt nostrud. Occaecat ullamco amet incididunt incididunt est culpa reprehenderit ex elit velit consectetur culpa culpa. Excepteur duis dolor nulla sint eu amet ut ex non dolore ut. Non sunt anim culpa duis ex dolore culpa enim cillum sint mollit reprehenderit. Eu velit incididunt est eiusmod quis excepteur. Consectetur minim non veniam pariatur nisi ea fugiat deserunt fugiat incididunt. Cillum adipisicing sunt excepteur occaecat laborum irure non veniam commodo eu ullamco esse exercitation. Commodo veniam ut cillum anim Lorem amet pariatur ea ullamco.',
+            picture: null
+        }
+    ]
+})
